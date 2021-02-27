9703bfcda1d5b3f86e1d3a2e33bf43838f8a5184
Antonio Galvan <kostalosh@gmail.com>
2021-02-27 21:23:14 +0100

    Initial Commit

---
+++ ListaCompra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListaCompra
+//
+//  Created by Antonio Galvan Ruiz on 27/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
