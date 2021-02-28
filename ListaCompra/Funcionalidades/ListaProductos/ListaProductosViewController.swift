5371974fdbe941115d1f4d756cd19bfcb549a9be
Antonio Galvan <kostalosh@gmail.com>
2021-02-28 01:15:18 +0100

    color de la categoría sin margen, para que ocupe todo el largo...

--- ListaCompra/Funcionalidades/ListaProductos/ListaProductosViewController.swift
+++ ListaCompra/Funcionalidades/ListaProductos/ListaProductosViewController.swift
@@ -42,8 +42,7 @@ final class ListaProductosViewController: UIViewController {
 
 extension ListaProductosViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 15
-        
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
